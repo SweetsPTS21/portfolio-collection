7a9ac52a0492cbbc34c2c5f54fc47487193806be
sonpt1 <sonpt1@vnpay.vn>
2026-05-15 10:51:20 +0700

    update yenvi-portfolio

--- yenvi-portfolio/src/pages/ProjectsPage.jsx
+++ yenvi-portfolio/src/pages/ProjectsPage.jsx
@@ -1,24 +1,20 @@
-import React from "react";
-import PageShell from "../components/layout/PageShell";
-import SpellCard from "../components/projects/SpellCard";
-import { projects } from "../data/portfolioData";
+import React from 'react';
+import PageShell from '../components/layout/PageShell';
+import SpellCard from '../components/projects/SpellCard';
+import { projects } from '../data/portfolioData';
 
 export default function ProjectsPage() {
-    return (
-        <PageShell eyebrow="Spellbook projects" title="Digital law spell cards">
-            <p className="lead">
-                Tập hợp các sản phẩm và quá trình nghiên cứu thể hiện sự giao
-                thoa giữa kỹ năng số và tư duy học thuật.
-            </p>
-            <section className="project-grid">
-                {projects.map((project, index) => (
-                    <SpellCard
-                        key={project.title}
-                        project={project}
-                        index={index}
-                    />
-                ))}
-            </section>
-        </PageShell>
-    );
+  return (
+    <PageShell eyebrow="Spellbook projects" title="Sản phẩm nghiên cứu">
+      <p className="lead">
+        Tập hợp các sản phẩm và quá trình nghiên cứu thể hiện sự giao thoa giữa
+        kỹ năng số và tư duy học thuật.
+      </p>
+      <section className="project-grid">
+        {projects.map((project, index) => (
+          <SpellCard key={project.title} project={project} index={index} />
+        ))}
+      </section>
+    </PageShell>
+  );
 }
