0ddb7540cd9bdcb07689229a90061baee3e5ced1
Pham Thanh Son <pts.28032001@gmail.com>
2026-05-20 20:41:48 +0700

    update khanh chi portfolio

--- kchi-portfolio/src/components/about/AvatarCard.jsx
+++ kchi-portfolio/src/components/about/AvatarCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import avatar from "../../assets/kitty-cafe-menu/avatar.jpg";
 import kittyMascot from "../../assets/kitty-cafe-menu/kitty-mascot.svg";
 import latteCup from "../../assets/kitty-cafe-menu/latte-cup.svg";
 
@@ -6,6 +7,7 @@ export default function AvatarCard() {
     return (
         <div className="avatar-card">
             <img className="avatar-kitty" src={kittyMascot} alt="" />
+            <img className="avatar" src={avatar} alt="Nguyễn Khánh Chi" />
             <img className="avatar-latte" src={latteCup} alt="" />
         </div>
     );
